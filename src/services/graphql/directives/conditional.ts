af101ee85e4518f78b93ece42ba8624e810715d3
Quentin <qlaffont13@gmail.com>
2024-04-02 14:48:20 +0200

    feat(gql): add rate limit

---
+++ src/services/graphql/directives/conditional.ts
@@ -0,0 +1,234 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable prefer-rest-params */
+
+export interface ClassDecorator {
+  <TFunction extends Function>(target: TFunction): TFunction | void;
+}
+
+export interface PropertyDecorator {
+  (target: Object, propertyKey: string | symbol): void;
+}
+
+export interface MethodDecorator {
+  <T>(
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: TypedPropertyDescriptor<T>,
+  ): TypedPropertyDescriptor<T> | void;
+}
+
+export interface ParameterDecorator {
+  (target: Object, propertyKey: string | symbol, parameterIndex: number): void;
+}
+
+/**
+ * Enum for decorator type
+ */
+export enum DecoratorType {
+  Class,
+  Parameter,
+  Property,
+  Method,
+  None,
+}
+
+/**
+ * Guesses which kind of decorator from its functional arguments
+ * @param args
+ * @returns {DecoratorType}
+ */
+export function getDecoratorTypeFromArguments(args: IArguments): DecoratorType {
+  'use strict';
+  if (args.length === 0 || args.length > 3) {
+    return DecoratorType.None;
+  }
+
+  const kind: string = typeof (args.length === 1 ? args[0] : args[2]);
+  switch (kind) {
+    case 'function':
+      return DecoratorType.Class;
+    case 'number':
+      return DecoratorType.Parameter;
+    case 'undefined':
+      return DecoratorType.Property;
+    case 'object':
+      return DecoratorType.Method;
+    default:
+      return DecoratorType.None;
+  }
+}
+
+/**
+ * Guesses whether the given function is a class decorator
+ * @param decorator
+ * @param args
+ * @returns {boolean}
+ */
+export function isClassDecorator(
+  decorator: Function,
+  args: IArguments,
+): decorator is ClassDecorator {
+  'use strict';
+  return getDecoratorTypeFromArguments(args) === DecoratorType.Class;
+}
+
+/**
+ * Guesses whether the given function is a method parameter decorator
+ * @param decorator
+ * @param args
+ * @returns {boolean}
+ */
+export function isParameterDecorator(
+  decorator: Function,
+  args: IArguments,
+): decorator is ParameterDecorator {
+  'use strict';
+  return getDecoratorTypeFromArguments(args) === DecoratorType.Parameter;
+}
+
+/**
+ * Guesses whether the given function is a property decorator
+ * @param decorator
+ * @param args
+ * @returns {boolean}
+ */
+export function isPropertyDecorator(
+  decorator: Function,
+  args: IArguments,
+): decorator is PropertyDecorator {
+  'use strict';
+  return getDecoratorTypeFromArguments(args) === DecoratorType.Property;
+}
+
+/**
+ * Guesses whether the given function is a method decorator
+ * @param decorator
+ * @param args
+ * @returns {boolean}
+ */
+export function isMethodDecorator(
+  decorator: Function,
+  args: IArguments,
+): decorator is MethodDecorator {
+  'use strict';
+  return getDecoratorTypeFromArguments(args) === DecoratorType.Method;
+}
+
+/**
+ * Apply `decorator` on a class if `test` is true
+ * @param test
+ * @param decorator
+ */
+function Conditional(test: boolean, decorator: ClassDecorator): ClassDecorator;
+/**
+ * Apply `decorator` on a class if `test` function returns true
+ * @param test function which receives a target class itself as an argument and returns boolean value
+ * @param decorator
+ */
+function Conditional(
+  test: (clazz?: Function) => boolean,
+  decorator: ClassDecorator,
+): ClassDecorator;
+
+/**
+ * Apply `decorator` on a property if `test` is true
+ * @param test
+ * @param decorator
+ */
+function Conditional(
+  test: boolean,
+  decorator: PropertyDecorator,
+): PropertyDecorator;
+/**
+ * Apply `decorator` on a property if `test` function returns true
+ * @param test function which receives a class' prototype and property name as arguments and returns boolean value
+ * @param decorator
+ */
+function Conditional(
+  test: (target?: Object, key?: string | symbol) => boolean,
+  decorator: PropertyDecorator,
+): PropertyDecorator;
+
+/**
+ * Apply `decorator` on a method parameter if `test` is true
+ * @param test
+ * @param decorator
+ */
+function Conditional(
+  test: boolean,
+  decorator: ParameterDecorator,
+): ParameterDecorator;
+/**
+ * Apply `decorator` on a method parameter if `test` function returns true
+ * @param test function which receives a class' prototype, property name and parameter position as arguments and returns boolean value
+ * @param decorator
+ */
+function Conditional(
+  test: (target?: Object, key?: string | symbol, index?: number) => boolean,
+  decorator: ParameterDecorator,
+): ParameterDecorator;
+
+/**
+ * Apply `decorator` on a method (which includes property accessor) if `test` is true
+ * @param test
+ * @param decorator
+ */
+function Conditional(
+  test: boolean,
+  decorator: MethodDecorator,
+): MethodDecorator;
+/**
+ * Apply `decorator` on a method (which includes property accessor) if `test` function returns true
+ * @param test function which receives a class' prototype, method name and property descriptor as arguments and returns boolean value
+ * @param decorator
+ */
+function Conditional(
+  test: (
+    target?: Object,
+    key?: string | symbol,
+    desc?: PropertyDescriptor,
+  ) => boolean,
+  decorator: MethodDecorator,
+): MethodDecorator;
+
+function Conditional(test: any, decorator: Function): any {
+  'use strict';
+  return function (target: Object, key: string | symbol, value: any): any {
+    if (isClassDecorator(decorator, arguments)) {
+      const clazz: Function = target as Function;
+      const shouldDecorate: boolean =
+        typeof test === 'function' ? test(clazz) : test;
+      if (shouldDecorate && decorator) {
+        return decorator(clazz);
+      }
+      return clazz;
+    }
+    if (isParameterDecorator(decorator, arguments)) {
+      const index: number = value as number;
+      const shouldDecorate: boolean =
+        typeof test === 'function' ? test(target, key, index) : test;
+      if (shouldDecorate && decorator) {
+        decorator(target, key, index);
+      }
+    }
+    if (isPropertyDecorator(decorator, arguments)) {
+      const shouldDecorate: boolean =
+        typeof test === 'function' ? test(target, key) : test;
+      if (shouldDecorate && decorator) {
+        decorator(target, key);
+      }
+    }
+    if (isMethodDecorator(decorator, arguments)) {
+      const desc: PropertyDescriptor = value as PropertyDescriptor;
+      const shouldDecorate: boolean =
+        typeof test === 'function' ? test(target, key, desc) : test;
+      if (shouldDecorate && decorator) {
+        return decorator(target, key, desc);
+      }
+      return desc;
+    }
+  };
+}
+
+export default Conditional;
---
+++ src/services/graphql/directives/rate-limit.ts
@@ -0,0 +1,19 @@
+import { rateLimitDirective as rld } from 'graphql-rate-limit-directive';
+import { Directive } from 'type-graphql';
+
+import { isPreProductionEnv, isProductionEnv } from '../../../services/env';
+import Conditional from './conditional';
+
+const { rateLimitDirectiveTypeDefs, rateLimitDirectiveTransformer } = rld();
+
+export const rateLimitDirective = {
+  typeDefs: rateLimitDirectiveTypeDefs,
+  transformer: rateLimitDirectiveTransformer,
+};
+
+export function RateLimitDirective(limit = 5, duration = 60) {
+  return Conditional(
+    (isProductionEnv() || isPreProductionEnv()) as boolean,
+    Directive(`@rateLimit(limit: ${limit}, duration: ${duration})`),
+  ) as MethodDecorator;
+}
