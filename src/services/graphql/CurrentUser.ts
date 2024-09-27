477d3e1c8c673fa822478ab4b74196f24f96047a
Quentin Laffont <qlaffont13@gmail.com>
2024-09-27 14:30:30 +0200

    fix: fix upgrade

--- src/services/graphql/CurrentUser.ts
+++ src/services/graphql/CurrentUser.ts
@@ -1,8 +1,8 @@
 import type { Users } from '@db/schemas';
-import { createParamDecorator } from 'type-graphql';
+import { createParameterDecorator } from 'type-graphql';
 
 export function CurrentUser() {
-  return createParamDecorator<{ user: typeof Users.$inferSelect }>(
+  return createParameterDecorator<{ user: typeof Users.$inferSelect }>(
     ({ context }) => {
       return context.user as typeof Users.$inferSelect;
     },
